8a5b08f921f09b7d067f76565044b246f1e02ce7
smurf123444 <smurf123444@gmail.com>
2019-11-02 10:58:32 -0400

    Added Files needed for game project.

---
+++ Source/BallRollingGameEditor.Target.cs
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class BallRollingGameEditorTarget : TargetRules
+{
+	public BallRollingGameEditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+
+		ExtraModuleNames.AddRange( new string[] { "BallRollingGame" } );
+	}
+}
