a5b8ef0939d1558bf4eadb6181f101db0e480565
Christopher Manning <cmanni12@student.scf.edu>
2021-06-23 11:13:02 -0700

    updated game engine to work

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -21,7 +21,7 @@ namespace Hazel {
 	{
 		while (m_Running)
 		{
-			glClearColor(1, 0, 1, 1);
+			glClearColor(1, 0, 0, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
---
+++ generateProject.bat
@@ -0,0 +1,2 @@
+call hazel\vendor\bin\premake5.exe vs2019
+PAUSE
